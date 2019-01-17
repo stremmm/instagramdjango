03d86ee56eb802a4b4fac2e38698833341ca02d5
Stephanie <stephaniestremick@gmail.com>
2019-01-17 16:03:03 -0700

    static add

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -29,7 +29,7 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = [
     #'8e41bf32.ngrok.io',
-    'djangoinstagram.herokuapp.com',
+    'instagramdjango.herokuapp.com',
     '127.0.0.1',
 ]
 
